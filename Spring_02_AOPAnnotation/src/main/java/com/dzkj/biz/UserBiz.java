025eb16f207c49026b1074c2d64ca9aeed89dde4
曾斌 <1132732682@qq.com>
2019-08-10 16:27:14 +0800

    第一次传递到服务器

---
+++ Spring_02_AOPAnnotation/src/main/java/com/dzkj/biz/UserBiz.java
@@ -0,0 +1,18 @@
+package com.dzkj.biz;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserBiz {
+
+	public void findAll() {
+		System.out.println("查询所有用户");
+	}
+	
+	public boolean delete (int id) {
+		System.out.println("商量出用户成功，id："+id);
+		System.out.println(1/0);
+		return true;
+	}
+}
+
